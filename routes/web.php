e9c5758f4a593794f159b33c06c3603f54c7a98d
webloit-dev <webloit2020@gmail.com>
2023-06-18 02:35:15 +0100

    testing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => config('imela.middleware', ['web'])], function () {
-    Route::post('change-email', [App\Http\Controllers\Auth\EmailFactory::class, 'changeEmailPost']);
+    Route::post('change-email', [Imela\Http\Controllers\Auth\EmailFactory::class, 'changeEmailPost']);
     
-    Route::get('change-email/confirmation', [App\Http\Controllers\Auth\EmailFactory::class, 'confirmChangeEmailLink'])->middleware(['signed'])->name('change-email.verify');
+    Route::get('change-email/confirmation', [Imela\Http\Controllers\Auth\EmailFactory::class, 'confirmChangeEmailLink'])->middleware(['signed'])->name('change-email.verify');
 });
--- src/Mail/ChangeEmail.php
+++ src/Mail/ChangeEmail.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Mail\Dashboard;
+namespace Imela\Mail\Dashboard;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
